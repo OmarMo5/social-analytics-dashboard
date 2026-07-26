8c3334d3c7df32d46bf0214308e53d8888b0fdfd
OmarMo5 <omarmo5tar12@gmail.com>
2026-07-26 13:34:27 +0300

    Adding New User

--- src/config/users.js
+++ src/config/users.js
@@ -7,6 +7,7 @@
 const USERS = [
   { name: 'Omar Mokhtar', email: 'omarmo@gmail.com', password: '123456789' },
   { name: 'Eng. Fahd', email: 'eng.fahd@asc.sa.com', password: '123456789' },
+  { name: 'Dr. Misfer Al-Wadai', email: 'dr@asc.sa.com', password: '$123456789$' },
   { name: 'social-analytic', email: 'social@asc.sa.com', password: '123456789' },
 ];
 
