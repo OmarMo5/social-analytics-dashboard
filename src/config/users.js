e406edaa0546bb6e3df14a041869030f08722578
OmarMo5 <omarmo5tar12@gmail.com>
2026-07-26 14:20:46 +0300

    Adding New UserNum4

--- src/config/users.js
+++ src/config/users.js
@@ -8,6 +8,7 @@ const USERS = [
   { name: 'Omar Mokhtar', email: 'omarmo@gmail.com', password: '123456789' },
   { name: 'Eng. Fahd', email: 'eng.fahd@asc.sa.com', password: '123456789' },
   { name: 'Dr. Misfer Al-Wadai', email: 'dr@asc.sa.com', password: '$123456789$' },
+  { name: 'Mr. Ahmed Elsayed', email: 'a.elsayed@asc.sa', password: 'Ahmad@321' },
   { name: 'social-analytic', email: 'social@asc.sa.com', password: '123456789' },
 ];
 
